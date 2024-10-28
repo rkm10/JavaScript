481ee09995e8a3f8c22cde4bd82f44a4d1bc0999
rajkumarmalluri <139203912+rkm10@users.noreply.github.com>
2024-10-28 13:23:11 +0000

    Datatypes

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,11 @@
+const account_Id = 96369;
+let accountEmail = "webwithraj@gmail.com";
+var accountPassword ="987654"
+accountCity ="Bangalore";
+
+console.log(accountEmail);
+accountEmail  = 56454;
+console.log(accountEmail);
+
+console.table([accountEmail,accountPassword,account_Id,accountCity]);
+
---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,14 @@
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/ false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+//null =>it is object type.
+//undefined => it is undefined type. 
+console.log(typeof undefined);
+
