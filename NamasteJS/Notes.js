cdd64a220fbea1e293b7fa2a5742906df3e75eaa
rajkumarmalluri <139203912+rkm10@users.noreply.github.com>
2024-10-30 14:27:18 +0530

    notes updated

--- NamasteJS/Notes.js
+++ NamasteJS/Notes.js
@@ -8,6 +8,24 @@
 5. Variable declarations are scanned and are made undefined.
 6. Function declarations are scanned and are made available.
 
-
+7. anything that is not inside an function is global space
+8. undefined means it is yet be assigned a value. as there is no value , JS replaces it with undefined as placeholder.
+9. Scope of a variable is directly dependent on the lexical environment.
+10. Whenever an execution context is created, a lexical environment is created. Lexical environment is the local memory along with the lexical environment of its parent. Lexical as a term means in hierarchy or in sequence.
+11. js use diff memory than global execution context to store let and cost. which is reason behind "temporal dead zone"
+12. let and const declarations are hoisted. we cant use them before initialization is result of "temporal dead zone".
+13.  level of strictness ... var<<let<<const.
+14. var //no temporal dead zone, can redeclare and re-initialize, stored in GES.
+    let //use TDZ, can't re-declare, can re-initialize, stored in separate memory.
+    const //use TDZ, can't re-declare, can't re-initialize, stored in separate memory.
+15. syntax error ... violation of JS syntax.
+    type error ...  while trying to re-initialize const variable.
+    reference error ... while trying to access variable which is not there in global memory.
+16. window.variable OR this.variable will not give value of variable defined using let or const.
+17. const used for static or not changed later.
+18. always add or put declarations the initializations on the top to aviod any temporal dead zone
+19. let and const are block scoped.
+    var is a gobal scope.
+20.lexical scope is nothing but a scope inside a scope.
 
 */
